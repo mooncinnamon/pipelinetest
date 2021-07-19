9c04220f0dc531b708177c3f5c75e1f415c390d7
mooncinnamon <moon.pinnamon@gmail.com>
2021-07-19 16:56:40 +0900

    외부포트 오픈

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
+host = '0.0.0.0'
 
 @app.route('/')
 def hello_world():
@@ -9,4 +10,4 @@ def hello_world():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host=host)
