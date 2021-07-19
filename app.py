e4c1fb06a0ae994dc5ea25af57c34a89e183a0c6
mooncinnamon <moon.pinnamon@gmail.com>
2021-07-19 17:05:40 +0900

    외부포트 닫으면 안됨

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+host = '0.0.0.0'
+
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host=host)
